6fcbc45edfba923cf070911e63af49ee896bfd5c
Egjfour <ejenkins20@jcu.edu>
2024-06-18 11:16:57 +0200

    Fix Settings

--- goodreads-predictor/src/goodreads_predictor/settings.py
+++ goodreads-predictor/src/goodreads_predictor/settings.py
@@ -31,8 +31,7 @@ CONFIG_LOADER_CLASS = OmegaConfigLoader
 # Keyword arguments to pass to the `CONFIG_LOADER_CLASS` constructor.
 CONFIG_LOADER_ARGS = {
       "base_env": "base",
-      "default_run_env": "local",
-      "test_env": "dev"
+      "default_run_env": "local"
 #       "config_patterns": {
 #           "spark" : ["spark*/"],
 #           "parameters": ["parameters*", "parameters*/**", "**/parameters*"],
