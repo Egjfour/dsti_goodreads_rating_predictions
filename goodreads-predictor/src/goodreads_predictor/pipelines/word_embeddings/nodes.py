c801f1f492f2d3dc2ff93534fdb7074976dbfd5c
Ilyes Azouani <ilyesazouani@gmail.com>
2024-06-28 19:07:54 +0200

    Built the logic for word embedding

--- goodreads-predictor/src/goodreads_predictor/pipelines/word_embeddings/nodes.py
+++ goodreads-predictor/src/goodreads_predictor/pipelines/word_embeddings/nodes.py
@@ -1,4 +1,12 @@
-"""
-This is a boilerplate pipeline 'word_embeddings'
-generated using Kedro 0.19.6
-"""
+# nodes.py
+
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+
+def create_embeddings(books_loaded):
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    df = pd.read_parquet(books_loaded)
+    df['title_embedding'] = df['title'].apply(lambda x: model.encode(x).tolist())
+    df['author_embedding'] = df['authors'].apply(lambda x: model.encode(x).tolist())
+    return df
+
