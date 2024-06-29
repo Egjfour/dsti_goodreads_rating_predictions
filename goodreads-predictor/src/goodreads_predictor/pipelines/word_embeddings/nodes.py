53d764304d9b13589ff1f0d37f41104a55edef83
Egjfour <ejenkins20@jcu.edu>
2024-06-29 14:02:34 +0200

    Eddie Updates

--- goodreads-predictor/src/goodreads_predictor/pipelines/word_embeddings/nodes.py
+++ goodreads-predictor/src/goodreads_predictor/pipelines/word_embeddings/nodes.py
@@ -1,12 +1,9 @@
 # nodes.py
 
-import pandas as pd
 from sentence_transformers import SentenceTransformer
 
 def create_embeddings(books_loaded):
     model = SentenceTransformer('all-MiniLM-L6-v2')
-    df = pd.read_parquet(books_loaded)
-    df['title_embedding'] = df['title'].apply(lambda x: model.encode(x).tolist())
-    df['author_embedding'] = df['authors'].apply(lambda x: model.encode(x).tolist())
-    return df
-
+    books_loaded['title_embedding'] = books_loaded['title'].apply(lambda x: model.encode(x).tolist())
+    books_loaded['author_embedding'] = books_loaded['authors'].apply(lambda x: model.encode(x).tolist())
+    return books_loaded
--- goodreads-predictor/src/goodreads_predictor/pipelines/word_embeddings/pipeline.py
+++ goodreads-predictor/src/goodreads_predictor/pipelines/word_embeddings/pipeline.py
@@ -6,5 +6,5 @@ from .nodes import create_embeddings
 
 def create_pipeline(**kwargs) -> Pipeline:
     return Pipeline([
-        node(func=create_embeddings, inputs=['books_loaded'], outputs='books_processed')
+        node(func=create_embeddings, inputs=['books_loaded'], outputs='books_embedded', name='apply_word_embeddings')
     ])
