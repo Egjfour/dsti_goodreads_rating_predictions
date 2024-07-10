9b575f9d6193698f38817a5d0a618f0d0acd8fbd
Ilyès <122605530+Ilyes-Azni@users.noreply.github.com>
2024-07-10 14:32:24 +0200

    Embeddings pipeline (#5)
    * Built the logic for word embedding
    
    * Eddie Updates
    
    * Updates
    
    * Updated Requirements
    
    * Ignore Pylint Error
    
    ---------
    
    Co-authored-by: Egjfour <ejenkins20@jcu.edu>

--- goodreads-predictor/src/goodreads_predictor/pipelines/word_embeddings/nodes.py
+++ goodreads-predictor/src/goodreads_predictor/pipelines/word_embeddings/nodes.py
@@ -1,4 +1,9 @@
-"""
-This is a boilerplate pipeline 'word_embeddings'
-generated using Kedro 0.19.6
-"""
+# nodes.py
+
+from sentence_transformers import SentenceTransformer # pylint: disable=E0401
+
+def create_embeddings(books_loaded):
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    books_loaded['title_embedding'] = books_loaded['title'].apply(lambda x: model.encode(x).tolist())
+    books_loaded['author_embedding'] = books_loaded['authors'].apply(lambda x: model.encode(x).tolist())
+    return books_loaded
