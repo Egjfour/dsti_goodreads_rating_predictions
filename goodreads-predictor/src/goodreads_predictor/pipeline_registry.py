20893a2415c3b630618da67ee19791f5c79d3f42
Ilyes <ilyesazouani@gmail.com>
2024-09-18 13:42:38 +0200

    Updated the pipeline registry to register the new pipeline

--- goodreads-predictor/src/goodreads_predictor/pipeline_registry.py
+++ goodreads-predictor/src/goodreads_predictor/pipeline_registry.py
@@ -3,7 +3,7 @@ from typing import Dict
 
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
-from .pipelines import data_load, word_embeddings, data_processing, model_data_creation, flaml_model
+from .pipelines import data_load, word_embeddings, data_processing, model_data_creation, flaml_model, pycaret_models
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -21,6 +21,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
     
     # Custom pipeline runs for the models
     flaml_model_pipeline = flaml_model.create_pipeline()
+    pycaret_model_pipeline = pycaret_models.create_pipeline()
 
     # Add the model pipelines to the pipelines dictionary
     pipelines["flaml_model"] = flaml_model_pipeline
@@ -29,6 +30,8 @@ def register_pipelines() -> Dict[str, Pipeline]:
     pipelines['full_data_load'] = data_load.create_pipeline() + word_embeddings.create_pipeline() # Import from blob and all external calls
     pipelines['build_model_data_from_loaded'] = data_processing.create_pipeline() + model_data_creation.create_pipeline() # Run all data processing without reloading data
     pipelines['load_and_build_model_data'] = pipelines.get('full_data_load') + pipelines.get('build_model_data_from_loaded') # Run all data processing with reloading data
-    pipelines['run_all_models'] = flaml_model_pipeline # Run all models -- add to this as more models are created
+    pipelines["flaml_model"] = flaml_model_pipeline
+    pipelines["pycaret_model"] = pycaret_model_pipeline
+    pipelines['run_all_models'] = flaml_model_pipeline + pycaret_model_pipeline # Run all models -- add to this as more models are created
 
     return pipelines
--- goodreads-predictor/src/goodreads_predictor/pipelines/pycaret_models/pipeline.py
+++ goodreads-predictor/src/goodreads_predictor/pipelines/pycaret_models/pipeline.py
@@ -3,8 +3,17 @@ This is a boilerplate pipeline 'pycaret_models'
 generated using Kedro 0.19.6
 """
 
-from kedro.pipeline import Pipeline, pipeline
-
+from kedro.pipeline import Pipeline, node
+from .nodes import train_and_predict_models
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return Pipeline(
+        [
+            node(
+                func=train_and_predict_models,
+                inputs=["train_data", "test_data", "params:features", "params:target", "params:model_config"],
+                outputs="model_predictions",
+                name="train_and_predict_models_node",
+            ),
+        ]
+    )
